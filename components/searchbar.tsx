f0dbcfe6f5931e3e2d6560a168607399c4ec7c18
Aarav-Singh2007 <aaravsingh7july2007@gmail.com>
2026-03-17 10:11:51 +0530

    Wanted commit

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -45,8 +45,9 @@ const Searchbar = ({ onPress, placeholder, value, onChangeText }: Props) => {
       <TouchableOpacity onPress={onPress} className="flex-row items-center bg-Dark-200 rounded-full px-5 py-4">
         <Image source={icons.search} className="w-5 h-5" resizeMode="contain" tintColor="#FFFFFF" />
         <Text className="flex-1 ml-2 text-white">{placeholder}</Text>
-      </TouchableOpacity>
+      </TouchableOpacity
     );
+    
   }
 
   // Otherwise render a text input (for search screen)
